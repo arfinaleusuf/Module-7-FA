34cc507add9c9588ce66327217a855910931c8f9
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-01 12:49:03 +0600

    creating user table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
 
 class Todos(Base):
 
@@ -10,3 +10,16 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+class user(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key= True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    hash_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
